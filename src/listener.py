d070d48e0844698279cfa09ee655425f60bcdfdc
Johan Mulder <johan@mulder.net>
2020-03-08 21:41:18 +0100

    Updated reader to include generated power
    All changes:
    - send_influx_data now requires the table parameter.
    - Added influx registration of total energy delivered on meter1+meter2.
    - Added influx registration of currently generated power.
    
    With this commit an additional table/measurement called "generated" is added.

--- src/listener.py
+++ src/listener.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import json
 from datetime import datetime
 
@@ -24,7 +25,7 @@ def on_connect(*args):
         print(f"Subscribed to {TOPIC} with message id {message_id}")
 
 
-def send_influx_data(meter, value, timestamp=None, table="usage"):
+def send_influx_data(meter, value, table, timestamp=None):
     if timestamp is None:
         t = datetime.now()
         # Round to 10 seconds
@@ -35,9 +36,9 @@ def send_influx_data(meter, value, timestamp=None, table="usage"):
         timestamp = t.timestamp()
 
     # Convert to string
-    timestamp = "%s%09d" % (str(int(timestamp)), 0)
+    timestamp = '%s%09d' % (str(int(timestamp)), 0)
 
-    post_data = "{0},meter={1} value={2} {3}".format(table, meter, value, timestamp)
+    post_data = '{0},meter={1} value={2} {3}'.format(table, meter, value, timestamp)
     print(f"table={table}, meter={meter}, value={value}, timestamp={timestamp}, url={INFLUXDB_URL}")
     requests.post(INFLUXDB_URL, data=post_data)
 
@@ -51,13 +52,25 @@ def on_message(*args):
     power_meter_time = energy_info.get('meter_time', None)
     gas_meter_time = energy_info.get('gas_last_measurement', None)
 
+    # Power usage
     current_power = energy_info.get('instantaneous_active_power_draw_l1', None)
-    if current_power:
-        send_influx_data("current", value=current_power, timestamp=power_meter_time, table="current_usage")
+    if current_power is not None:
+        send_influx_data('current', value=current_power, timestamp=power_meter_time, table='current_usage')
+    # Solar panels
+    current_power_delivery = energy_info.get('instantaneous_active_power_delivery_l1', None)
+    if current_power_delivery is not None:
+        send_influx_data('current', value=current_power_delivery, timestamp=power_meter_time, table='current_delivery')
 
-    send_influx_data('meter1', value=energy_info.get('total_usage_night'), timestamp=power_meter_time)
-    send_influx_data('meter2', value=energy_info.get('total_usage_day'), timestamp=power_meter_time)
-    send_influx_data('gas', value=energy_info.get('gas_usage_total'), timestamp=gas_meter_time)
+    # Consumed power meters. meter1 is low (night) tariff, meter2 is high (day) tariff.
+    send_influx_data('meter1', value=energy_info.get('total_usage_night'), timestamp=power_meter_time, table='usage')
+    send_influx_data('meter2', value=energy_info.get('total_usage_day'), timestamp=power_meter_time, table='usage')
+    # Generated power meters
+    send_influx_data('meter1', value=energy_info.get('total_energy_delivered_night'),
+                     timestamp=power_meter_time, table='generated')
+    send_influx_data('meter2', value=energy_info.get('total_energy_delivered_day'),
+                     timestamp=power_meter_time, table='generated')
+    # Gas usage.
+    send_influx_data('gas', value=energy_info.get('gas_usage_total'), timestamp=gas_meter_time, table='usage')
 
 
 def validate_env(env_key, value, expected_type):
@@ -77,8 +90,6 @@ if __name__ == '__main__':
 
     client.connect(host=MQTT_HOST, port=MQTT_PORT, keepalive=MQTT_KEEPALIVE)
 
-    # Blocking call that processes network traffic, dispatches callbacks and
-    # handles reconnecting.
-    # Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
+    # Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a manual interface.
     client.loop_forever()
