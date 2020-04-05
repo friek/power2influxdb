82dbf8dec26b9e33df24882cd384444cf1a872a3
Johan Mulder <johan@mulder.net>
2020-04-05 12:03:24 +0200

    Added power interval storage. Added influx readings measurement.
    A separate measurement is now created named "measurement", which
    stores the output of all meters, as well as the power exported and
    used over the last interval.

--- src/listener.py
+++ src/listener.py
@@ -5,8 +5,16 @@ from datetime import datetime
 
 import paho.mqtt.client as mqtt
 import requests
+from influxdb import InfluxDBClient
 
-from settings import MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE, TOPIC, INFLUXDB_URL
+from settings import MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE, TOPIC, INFLUXDB_URL, INFLUXDB_HOST, INFLUXDB_PORT, \
+    INFLUXDB_DB
+
+INFLUX_CLIENT = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT, database=INFLUXDB_DB)
+prev_totals = {
+    'used': None,
+    'exported': None,
+}
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -52,25 +60,58 @@ def on_message(*args):
     power_meter_time = energy_info.get('meter_time', None)
     gas_meter_time = energy_info.get('gas_last_measurement', None)
 
+    fields = {}
+    points = [
+        {
+            "measurement": "readings",
+            "time": power_meter_time,
+            "fields": fields
+        }
+    ]
+
     # Power usage
     current_power = energy_info.get('instantaneous_active_power_draw_l1', None)
     if current_power is not None:
         send_influx_data('current', value=current_power, timestamp=power_meter_time, table='current_usage')
+        fields['current_power'] = int(current_power * 1000)
     # Solar panels
     current_power_delivery = energy_info.get('instantaneous_active_power_delivery_l1', None)
     if current_power_delivery is not None:
         send_influx_data('current', value=current_power_delivery, timestamp=power_meter_time, table='current_delivery')
+        fields['current_export'] = int(current_power_delivery * 1000)
 
     # Consumed power meters. meter1 is low (night) tariff, meter2 is high (day) tariff.
     send_influx_data('meter1', value=energy_info.get('total_usage_night'), timestamp=power_meter_time, table='usage')
+    fields['meter1_total_usage'] = energy_info.get('total_usage_night')
     send_influx_data('meter2', value=energy_info.get('total_usage_day'), timestamp=power_meter_time, table='usage')
+    fields['meter2_total_usage'] = energy_info.get('total_usage_day')
     # Generated power meters
     send_influx_data('meter1', value=energy_info.get('total_energy_delivered_night'),
                      timestamp=power_meter_time, table='generated')
+    fields['meter1_total_exported'] = energy_info.get('total_energy_delivered_night')
     send_influx_data('meter2', value=energy_info.get('total_energy_delivered_day'),
                      timestamp=power_meter_time, table='generated')
+    fields['meter2_total_exported'] = energy_info.get('total_energy_delivered_day')
+
     # Gas usage.
     send_influx_data('gas', value=energy_info.get('gas_usage_total'), timestamp=gas_meter_time, table='usage')
+    fields['gas_usage_total'] = energy_info.get('gas_usage_total')
+
+    total_used = int(fields['meter1_total_usage'] * 1000) + int(fields['meter2_total_usage'] * 1000)
+    total_exported = int(fields['meter1_total_exported'] * 1000) + int(fields['meter2_total_exported'] * 1000)
+
+    if prev_totals['used'] is not None and prev_totals['exported'] is not None:
+        # noinspection PyTypeChecker
+        fields['power_exported'] = total_exported - prev_totals['exported']
+        # noinspection PyTypeChecker
+        fields['power_used'] = total_used - prev_totals['used']
+
+    INFLUX_CLIENT.write_points(points)
+
+    # noinspection PyTypeChecker
+    prev_totals['used'] = total_used
+    # noinspection PyTypeChecker
+    prev_totals['exported'] = total_exported
 
 
 def validate_env(env_key, value, expected_type):
